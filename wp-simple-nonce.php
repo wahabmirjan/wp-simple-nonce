c855898d7f24d26475bd9e8052ed312dd85585fd
Cal Evans <cal@calevans.com>
2014-05-28 07:30:40 -0500

    Initial checkin of scaffolding

---
+++ wp-simple-nonce.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wp-simple-nonce
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: wp-simple-nonce
+Domain Path: /languages
+*/
+
